c0a13a4438ff73a625dc76985edc8ee570a886bb
Ulises <ulises_astrada@outlook.com>
2021-03-21 22:21:24 -0300

    Initial commit

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,13 @@
+var hello = 'Hello'
+
+let world = 'Hello world'
+
+const helloWorld = 'Hello World!'
+
+const anotherFn = () => {
+  console.log(hello)
+  console.log(world)
+  console.log(helloWorld)
+}
+
+anotherFn()
